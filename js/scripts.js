388cc13454b0d56bce35d27aaa5e0aae60153973
123azemak <123azemak@frhsd.com>
2022-04-04 10:58:04 -0400

    Added JS funcionality and edited the order form

--- js/scripts.js
+++ js/scripts.js
@@ -51,9 +51,21 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#menu a.portfolio-box'
-    });
-
+document.getElementById("banana").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Banana Special";
+});
+document.getElementById("pizza").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Pepperoni Pizza";
+});
+document.getElementById("masterpiece").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Masterpiece";
+});
+document.getElementById("rice").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Rice Roundhouse";
+});
+document.getElementById("tot").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Tator tots";
+});
+document.getElementById("meat").addEventListener("click", function(){
+   document.getElementById("orderText").textContent = "You selected an order of: Mystery Meat";
 });
