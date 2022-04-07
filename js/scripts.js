2ba8b5fd8176612b19163e5ff33551435dedee5d
123azemak <123azemak@frhsd.com>
2022-04-07 10:23:46 -0400

    FIXED THE JAVASCRIPT

--- js/scripts.js
+++ js/scripts.js
@@ -51,21 +51,3 @@ window.addEventListener('DOMContentLoaded', event => {
         });
     });
 // javascript momemnt down below
-document.getElementById("banana").addEventListener('click', function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Banana Special";
-});
-document.getElementById("pizza").addEventListener("click", function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Pepperoni Pizza";
-});
-document.getElementById("masterpiece").addEventListener("click", function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Masterpiece";
-});
-document.getElementById("rice").addEventListener("click", function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Rice Roundhouse";
-});
-document.getElementById("tot").addEventListener("click", function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Tator tots";
-});
-document.getElementById("meat").addEventListener("click", function(){
-   document.getElementById("orderText").textContent = "You selected an order of: Mystery Meat";
-});
